7291cfc8a185e0dfacbd24a3428ada49f4e75d0e
kyouma_san <rohananand56@gmail.com>
2022-01-24 02:55:03 +0530

    adding buttons to move right/left

--- script.js
+++ script.js
@@ -5,18 +5,19 @@ both = 0;
 
 function moveLeft() {
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if(left>0){
+    if (left > 0) {
         character.style.left = left - 2 + "px";
     }
-   
+
 }
 
 function moveRight() {
+    console.log("right")
     var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
-    if(left<380){
+    if (left < 380) {
         character.style.left = left + 2 + "px";
     }
-   
+
 }
 
 
@@ -40,13 +41,33 @@ document.addEventListener("keyup", event => {
     both = 0;
 })
 
+
+function keepMovingRight(){
+    if(both == 0){
+        both = 1;
+        interval = setInterval(moveRight, 1);
+    }
+    
+}
+function keepMovingLeft(){
+    if(both == 0){
+        both = 1;
+        interval = setInterval(moveLeft, 1);
+    }
+    
+}
+function stopMoving(){
+    clearInterval(interval);
+    both = 0;
+}
+
 var block = document.createElement("div");
 var hole = document.createElement("div");
 block.setAttribute("class", "block")
 hole.setAttribute("class", "hole")
 block.setAttribute("id", "block")
 hole.setAttribute("id", "hole")
-var random = Math.floor(Math.random()*360)
+var random = Math.floor(Math.random() * 360)
 game.appendChild(block);
 game.appendChild(hole);
 
@@ -63,22 +84,22 @@ game.appendChild(hole);
 
 ////adding touch support
 
-function swipedetect(el, callback){
-  
+function swipedetect(el, callback) {
+
     var touchsurface = el,
-    swipedir,
-    startX,
-    startY,
-    distX,
-    distY,
-    threshold = 60, //required min distance traveled to be considered swipe
-    restraint = 100, // maximum distance allowed at the same time in perpendicular direction
-    allowedTime = 300, // maximum time allowed to travel that distance
-    elapsedTime,
-    startTime,
-    handleswipe = callback || function(swipedir){}
-  
-    touchsurface.addEventListener('touchstart', function(e){
+        swipedir,
+        startX,
+        startY,
+        distX,
+        distY,
+        threshold = 60, //required min distance traveled to be considered swipe
+        restraint = 100, // maximum distance allowed at the same time in perpendicular direction
+        allowedTime = 300, // maximum time allowed to travel that distance
+        elapsedTime,
+        startTime,
+        handleswipe = callback || function (swipedir) { }
+
+    touchsurface.addEventListener('touchstart', function (e) {
         var touchobj = e.changedTouches[0]
         swipedir = 'none'
         dist = 0
@@ -87,22 +108,22 @@ function swipedetect(el, callback){
         startTime = new Date().getTime() // record time when finger first makes contact with surface
         e.preventDefault()
     }, false)
-  
-    touchsurface.addEventListener('touchmove', function(e){
+
+    touchsurface.addEventListener('touchmove', function (e) {
         e.preventDefault() // prevent scrolling when inside DIV
     }, false)
-  
-    touchsurface.addEventListener('touchend', function(e){
+
+    touchsurface.addEventListener('touchend', function (e) {
         var touchobj = e.changedTouches[0]
         distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
         distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
         elapsedTime = new Date().getTime() - startTime // get time elapsed
-        if (elapsedTime <= allowedTime){ // first condition for awipe met
-            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint){ // 2nd condition for horizontal swipe met
-                swipedir = (distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+        if (elapsedTime <= allowedTime) { // first condition for awipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0) ? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
             }
-            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint){ // 2nd condition for vertical swipe met
-                swipedir = (distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+                swipedir = (distY < 0) ? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
             }
         }
         handleswipe(swipedir)
@@ -111,13 +132,28 @@ function swipedetect(el, callback){
 }
 
 
-window.addEventListener('load', function(){
+window.addEventListener('load', function () {
     var el = document.getElementById('game')
-    
 
-    swipedetect(el, function(swipedir){
-        if (swipedir != 'none'){
-            
+
+    swipedetect(el, function (swipedir) {
+        if (swipedir != 'none') {
+
+            if (swipedir == 'left') {
+                var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+                if (left > 0) {
+                    character.style.left = left - 10 + "px";
+                }
+            }
+
+            if (swipedir == 'right'){
+                var left = parseInt(window.getComputedStyle(character).getPropertyValue("left"));
+                if (left < 380) {
+                    character.style.left = left + 10 + "px";
+                }
+            }
+           
+
 
         }
     })
